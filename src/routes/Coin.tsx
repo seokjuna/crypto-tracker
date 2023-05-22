50800dcf9c4acce56318dc3e31dee90b01f6a9d8
naseokju <lukas95@naver.com>
2023-05-22 17:44:01 +0900

    Price Chart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -201,7 +201,7 @@ function Coin() {
                     </Tabs>
                     <Routes>
                         <Route path="price" element={<Price />} />
-                        <Route path="chart" element={<Chart />} />
+                        <Route path="chart" element={<Chart coinId={coinId} />} />
                     </Routes>
                 </>
             )}
