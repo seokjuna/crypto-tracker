a4f0eb4e8b435d1f13c0f87d55a6c8b0bc3d75a1
naseokju <lukas95@naver.com>
2023-05-22 17:34:36 +0900

    React Query part One

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -51,7 +52,7 @@ const Loader = styled.span`
     display: block;
 `;
 
-interface CoinInterface {
+interface ICoin {
     id: string,
     name: string,
     symbol: string,
@@ -62,6 +63,8 @@ interface CoinInterface {
 }
 
 function Coins() {
+    const { isLoading, data } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
+    /*
     const [coins, setCoins] = useState<CoinInterface[]>([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
@@ -72,17 +75,18 @@ function Coins() {
             setLoading(false);
         })();
     }, []);
+    */
 
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            {loading ? (
+            {isLoading ? (
                 <Loader>Loading...</Loader>
             ) : (
             <CoinList>
-                {coins.map((coin) => (
+                {data?.slice(0,100).map((coin) => (
                     <Coin key={coin.id}>
                         <Link to={`/${coin.id}`} state={{ name: coin.name }}>
                             <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
